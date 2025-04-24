f529b9079c4fb76e5fcc0e0aa37307e1f740ffe8
Lauberr <barbaralauber3@gmail.com>
2025-04-24 09:23:01 -0300

    preparando para merge

--- livraria_jambinha/src/App.jsx
+++ livraria_jambinha/src/App.jsx
@@ -1,11 +1,16 @@
 
 import React from 'react';
 import LoginForm from './pages/loginBiblio';  
+import { Home } from './pages/Home';
+import { Sidebar } from './components/Sidebar';
+import { Layout } from './pages/Layout';
 
 const App = () => {
   return (
     <div className="App">
       <LoginForm />
+      {/* <Home/> */}
+      <Layout/>
     </div>
   );
 }
